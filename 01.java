20713fafa9fbb8ff5832ae48eaed7087a5b3e1bb
Dennis Rafaell De Los Santos <155126204+Vizirioned@users.noreply.github.com>
2026-01-13 13:43:06 +0800

    Add files via upload

--- 01.java
+++ 01.java
@@ -1,97 +1,73 @@
 import java.util.Scanner;
+
+
 public class Main{
-     public static void main(String[] args) {
-         
-         boolean reservation = true;
-         Scanner res = new Scanner(System.in);
-         
-         
-         
-         String[] RowColumn;
-          String[] Rows = { "Row 1 ",
-                            "Row 2 ",
-                            "Row 3 ",
-                            "Row 4 ",
-                            "Row 5 ",
-                            "Row 6 ",
-                            "Row 7 ",
-                            "Row 8 ",
-                            "Row 9 ",
-                            "Row 10 "
-              
-          };
+    
+    public static void main(String[]args)   {
+        int rate, OT, hours;
+        String Name;
+        double paybasic, otpay, ptimepay, ratehour, grosspay, ftimepay; 
+        char PorF;
+        Scanner bes = new Scanner(System.in);
+    // name
+        System.out.print("Enter employee name: ");
+        Name = bes.nextLine();
+        //p or f
+    System.out.print("Press F for Full Time or P for Part Time: ");
+    PorF = bes.next().charAt(0);
+    
+    switch(PorF){
         
+        //part time
         
-        String [][] seatsTable = {
-                {"|*", "*","*", "*"},
-                {"|*", "*","*", "*"},
-                {"|*", "*","*", "*"},   
-                {"|*", "*","*", "*"},
-                {"|*", "*","*", "*"},
-                {"|*", "*","*", "*"},
-                {"|*", "*","*", "*"},
-                {"|*", "*","*", "*"},
-                {"|*", "*","*", "*"},
-                {"|*", "*","*", "*"}
-
-        };
-         
-
-         
-        do{ 
-           System.out.println("Bus Reservation");
-          String[] columns = { "Col 1",
-          "Col 2",
-          "Col 3",
-          "Col 4" 
-              
-          };
-          
-          
-        for(int i = 0; i < columns.length; i++)
-        {
-            System.out.print("\t" + columns[i]);
-
-        }
-
-       
-        System.out.println();   
+        case 'P':
         
-        for(int row = 0; row < seatsTable.length; row++)
-        {
-            System.out.print(Rows[row]);
-            
-            for(int col = 0; col < seatsTable[row].length; col++)
-            {
+         System.out.println("---Part Time Employee---");
+         System.out.print("Enter rate per hour: ");
+         ratehour = bes.nextDouble();
+         System.out.print("Enter no. of hours worked: ");
+         hours = bes.nextInt();
+         System.out.print("Enter no. of overtime: ");
+         OT = bes.nextInt();
+              System.out.println("________________________________");
+                System.out.println("Employee Name:      "+ Name);
+                  ptimepay = hours*ratehour;
+                System.out.println("Basic Pay:          "+ ptimepay);
+                  otpay = OT*(ratehour*1.25);
+                System.out.println("Ovetime Pay:        "+ otpay);
+              System.out.println("________________________________");
+              grosspay = ptimepay + otpay;
+                System.out.print("Gross Pay:          "+ grosspay);
+                break;
                 
-                System.out.print("\t" + seatsTable[row][col]);
-
-            }
-
-            System.out.println();
-        }
-
-
-        System.out.print("Enter row and column number to reserve separated by space (Enter a negative number to exit): ");
-        
-        RowColumn = res.nextLine().split(" ");
-        
-        int row1 = Integer.parseInt(RowColumn[0]);
-        int col2 = Integer.parseInt(RowColumn[1]);
-        if(Integer.parseInt(RowColumn[0]) > 0 && Integer.parseInt(RowColumn[1]) > 0){
-             seatsTable[row1-1][col2-1] = "x";
-        }
         
         
+        //full time
         
-        else{
-        reservation = false;
-
-        }
+        case 'F':
+            System.out.println("---Full Time Employee---");
+            System.out.print("Enter Basic Pay: ");
+            paybasic = bes.nextDouble();
+              System.out.println("________________________________");
+                 System.out.println("Employee Name:     "+ Name);
+                 System.out.println("Basic Pay:         "+ paybasic);
+              System.out.println("________________________________");
+                     ftimepay = paybasic;
+                 System.out.print("Gross Pay:         "+ ftimepay);
+                 break;
+                  
+                   
+                   
+                   default:
+                   
+                 System.out.println("Please type the right input");
+            
+            
+            
         
-        
-        }
-        while(reservation == true);
-        
-        }
+    }
+    
+    
+    
+    }
 }
